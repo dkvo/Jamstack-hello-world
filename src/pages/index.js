3fdeb6bcd1b7827a403466a77731bb6b96b60b69
dkvo <dangkhoanvo@gmail.com>
2018-10-24 01:51:02 -0700

    modified index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -7,9 +7,10 @@ import Image from '../components/image'
 const IndexPage = () => (
   <Layout>
     <h1>Hi everyone,</h1>
-    <p>My name is Khoa Vo. I'm a recent graduate from San Jose State University with a degree in Computer Science. 
-      I made this web application in order to introduce myself as well as acquiring the free ticket to the 2018 JAMstack Hackathon event. 
-      I look foward to being able to meet new people and improve on my coding skill sets.</p>
+    <p>My name is Khoa Vo. I'm a recent graduate from San Jose State University with a degree in Computer Science. My programming stack include 
+      Java, Python, Javascript, HTML, CSS, React.js, Node.js, SQL, PostgreSQL, and MongoDB.I made this web application in order to introduce 
+      myself as well as acquiring the free ticket to the 2018 JAMstack Hackathon event. I look foward to being able to meet new people and improve 
+      on my coding skill sets.</p>
   </Layout>
 )
 
