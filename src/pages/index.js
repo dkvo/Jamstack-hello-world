601216b401047d71b29c5ffb908c43937e33f20f
dkvo <dangkhoanvo@gmail.com>
2018-10-24 01:36:56 -0700

    initial commit

---
+++ src/pages/index.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import { Link } from 'gatsby'
+
+import Layout from '../components/layout'
+import Image from '../components/image'
+
+const IndexPage = () => (
+  <Layout>
+    <h1>Hi everyone,</h1>
+    <p>My name is Khoa Vo. I'm a recent graduate from San Jose State University with a degree in Computer Science. 
+      I made this web application in order to introduce myself as well as acquiring the free ticket to the 2018 JAMstack Hackathon event. 
+      I look foward to being able to meet new people and improve on my coding skill sets.</p>
+  </Layout>
+)
+
+export default IndexPage
