4199c99ed14adf9905b1e61793c6b98a5f9ad164
dkvo <dangkhoanvo@gmail.com>
2018-10-24 01:52:06 -0700

    modified index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -8,8 +8,8 @@ const IndexPage = () => (
   <Layout>
     <h1>Hi everyone,</h1>
     <p>My name is Khoa Vo. I'm a recent graduate from San Jose State University with a degree in Computer Science. My programming stack include 
-      Java, Python, Javascript, HTML, CSS, React.js, Node.js, SQL, PostgreSQL, and MongoDB.I made this web application in order to introduce 
-      myself as well as acquiring the free ticket to the 2018 JAMstack Hackathon event. I look foward to being able to meet new people and improve 
+      Java, Python, Javascript, HTML, CSS, React.js, Node.js, SQL, PostgreSQL, and MongoDB. I made this web application in order to introduce 
+      myself as well as trying to acquire the ticket to the 2018 JAMstack Hackathon event. I look foward to being able to meet new people and improve 
       on my coding skill sets.</p>
   </Layout>
 )
